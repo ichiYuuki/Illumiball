722aa588d70e5af22be942b1bec228e7d9583cff
ichiYuuki <mrofushi@gmail.com>
2016-01-12 12:21:39 +0900

    最初

---
+++ Assets/Scripts/FallInChecker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallInChecker : MonoBehaviour {
+
+    public Hole red;
+    public Hole blue;
+    public Hole green;
+
+    void OnGUI() {
+        string label = " ";
+
+        if (red.IsFallIn() && blue.IsFallIn() && green.IsFallIn())
+        {
+            label = "Fall in hole!";
+        }
+        GUI.Label(new Rect(0, 0, 100, 30), label);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
